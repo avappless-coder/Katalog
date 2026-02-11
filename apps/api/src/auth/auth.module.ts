6eecee108cab61235161bd2d428f772f847eba03
avappless-coder <avappless@gmail.com>
2026-02-11 04:26:33 +0200

    fix auth module mailer dependency

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
-﻿import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { MailerModule } from '../mailer/mailer.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({})],
+  imports: [PassportModule, JwtModule.register({}), MailerModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService]
